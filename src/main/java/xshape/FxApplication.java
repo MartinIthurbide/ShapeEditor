5f8cf4f581d3e8d554a491cca5a798f66245aa12
Vercasson Victor <vercasson1605@gmail.com>
2023-04-02 22:03:00 +0200

    test webhook

--- src/main/java/xshape/FxApplication.java
+++ src/main/java/xshape/FxApplication.java
@@ -1,16 +1,18 @@
 package xshape;
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FxApplication extends Application {
-    public static Group _root = new Group ();
+    public static Group _root = new Group();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("XShape JavaFx Rendering");
         Scene _scene = new Scene(_root, 500, 500);
+        _scene.setFill(javafx.scene.paint.Color.RED);
         primaryStage.setScene(_scene);
         primaryStage.show();
     }
